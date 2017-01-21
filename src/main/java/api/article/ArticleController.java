3cf5686b2dd26415ff9a39d8922837803a8742e1
Marc B <marc.benzahra@gmail.com>
2017-01-21 13:51:55 +0100

    Article controller first draft

--- src/main/java/api/article/ArticleController.java
+++ src/main/java/api/article/ArticleController.java
@@ -49,22 +49,186 @@ public class ArticleController {
   }
 
   public static void create(RoutingContext rc) {
-    rc.response().end("article/create");
+    JsonObject body = ctx.getBodyAsJson();
+    System.out.println("params: " + ctx.request().params());
+    System.out.println("body: " + body);
+
+    ArticleModel model = JsonHandler.getInstance().fromJson(ctx.getBodyAsJson().toString(), ArticleModel.class);
+
+    if (!model.validate()) {
+      System.out.println("articleModel validation failed");
+
+      ctx.response()
+      .setStatusCode(HttpURLConnection.HTTP_BAD_REQUEST)
+      .putHeader("content-type", "application/json; charset=utf-8")
+      .end(new JsonObject().put("message", "model validation failed").encodePrettily());
+      return ;
+    }
+
+    System.out.println("model toString: " + model);
+
+    MongoDB.getInstance().getClient().insert("articles", model.toJSON(), res -> {
+      if (res.succeeded()) {
+        System.out.println("res: " + res.result());
+        ctx.response()
+        .setStatusCode(HttpURLConnection.HTTP_OK)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(new JsonObject().put("id", res.result()).encodePrettily());
+        return ;
+      }
+      else {
+        System.out.println("FAIL: " + res.cause().getMessage());
+        ctx.fail(HttpURLConnection.HTTP_INTERNAL_ERROR);
+        return ;
+      }
+    });
   }
 
   public static void show(RoutingContext rc) {
-    rc.response().end("article/show");
+    String id = ctx.request().getParam("id");
+
+    MongoDB.getInstance().getClient().findOne(
+      "articles",
+      new JsonObject().put("_id", id),
+      new JsonObject().put("_id", false),
+      res -> {
+        if (res.succeeded()) {
+          JsonObject article = res.result();
+
+          System.out.println("article found: " + article);
+
+          if (article == null) {
+            article = new JsonObject().put("message", "id not found in db");
+            ctx.response().setStatusCode(HttpURLConnection.HTTP_NOT_FOUND);
+          }
+          else {
+            ctx.response().setStatusCode(HttpURLConnection.HTTP_OK);
+          }
+
+          ctx.response()
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(credential.encodePrettily());
+          return ;
+        }
+        else {
+          System.out.println("FAIL: " + res.cause().getMessage());
+          ctx.fail(HttpURLConnection.HTTP_INTERNAL_ERROR);
+          return ;
+        }
+      }
+    );
   }
 
   public static void update(RoutingContext rc) {
-    rc.response().end("article/update");
+    JsonObject body = ctx.getBodyAsJson();
+    System.out.println("params: " + ctx.request().params());
+    System.out.println("body: " + body);
+    String id = ctx.request().getParam("id");
+    JsonObject query = new JsonObject();
+    query.put("_id", id);
+    JsonObject update = new JsonObject();
+
+    for (String rField : ArticleModel.requiredFields) {
+      if (body.containsKey(rField)) {
+        update.put(rField, body.getString(rField));
+      }
+    }
+    for (String oField : ArticleModel.optionalFields) {
+      if (body.containsKey(oField)) {
+        update.put(oField, body.getString(oField));
+      }
+    }
+
+    update = new JsonObject().put("$set", update);
+
+    System.out.println("updated value :" + update);
+
+    MongoDB.getInstance().getClient().updateCollection(
+      "articles",
+      query,
+      update,
+      res -> {
+        if (res.succeeded()) {
+          JsonObject result = res.result().toJson();
+
+          System.out.println("result: " + result);
+
+          ctx.response()
+          .setStatusCode(HttpURLConnection.HTTP_NO_CONTENT)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end();
+          return ;
+        }
+        else {
+          System.out.println("FAIL: " + res.cause().getMessage());
+          ctx.fail(HttpURLConnection.HTTP_INTERNAL_ERROR);
+          return ;
+        }
+      }
+    );
   }
 
   public static void delete(RoutingContext rc) {
-    rc.response().end("article/delete");
+    String id = ctx.request().getParam("id");
+
+    MongoDB.getInstance().getClient().removeDocuments(
+      "articles",
+      new JsonObject().put("_id", id),
+      res -> {
+        if (res.succeeded()) {
+          JsonObject result = res.result().toJson();
+
+          System.out.println("result: " + result);
+
+          ctx.response()
+          .setStatusCode(HttpURLConnection.HTTP_NO_CONTENT)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end();
+          return ;
+        }
+        else {
+          System.out.println("FAIL: " + res.cause().getMessage());
+          ctx.fail(HttpURLConnection.HTTP_INTERNAL_ERROR);
+          return ;
+        }
+      }
+    );
   }
 
   public static void list(RoutingContext rc) {
-    rc.response().end("article/list");
+    MongoDB.getInstance().getClient().runCommand(
+      "find",
+      new JsonObject()
+      .put("find", "articles"),
+      res -> {
+        if (res.succeeded()) {
+          JsonObject result = res.result();
+          JsonArray articles = result.getJsonObject("cursor").getJsonArray("firstBatch");
+
+          System.out.println("all articles: " + articles);
+
+          if (articles == null || articles.size() <= 0) {
+            ctx.response()
+            .setStatusCode(HttpURLConnection.HTTP_NOT_FOUND)
+            .putHeader("content-type", "application/json; charset=utf-8")
+            .end(new JsonObject().put("message", "no article found").encodePrettily());
+            return ;
+          }
+          else {
+            ctx.response().setStatusCode(HttpURLConnection.HTTP_OK);
+          }
+
+          ctx.response()
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(articles.encodePrettily());
+          return ;
+        }
+        else {
+          System.out.println("FAIL: " + res.cause().getMessage());
+          ctx.fail(HttpURLConnection.HTTP_INTERNAL_ERROR);
+          return ;
+        }
+      }
+    );
   }
 }
