d74b320366312cf2f8d8a3aed51a9f8addfeb4af
Marc B <marc.benzahra@gmail.com>
2017-01-29 02:50:45 +0100

    add feed initialization

--- src/main/java/worker/Worker.java
+++ src/main/java/worker/Worker.java
@@ -1,8 +1,18 @@
 package urss.server.worker;
 
+import java.util.List;
+
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.buffer.Buffer;
+
+import urss.server.Server;
+import urss.server.components.MongoDB;
+
 public class Worker {
   private static final long defaultDelay = 10000;//10 seconds
   private long delay;
+  private HttpClient httpClient = null;
 
   public Worker() {
     this(Worker.defaultDelay);
@@ -12,8 +22,54 @@ public class Worker {
     setDelay(delay);
   }
 
+  public void initFeeds() {
+    if (httpClient == null) {
+      this.httpClient = Server.getInstance().getVertx().createHttpClient();
+    }
+
+    MongoDB.getInstance().getClient().find(
+      "urls",
+      new JsonObject(),
+      res -> {
+        if (res.succeeded()) {
+          List<JsonObject> results = res.result();
+
+          for (JsonObject feedUrl : results) {
+            System.out.println("meh: " + feedUrl);
+            this.httpClient.post(4242, "localhost", "/api/feeds/fromURL",
+                                 response -> {
+                                   response.handler(body -> {
+                                     System.out.println("response body: " + body);
+                                   });
+                                 })
+                                 .end(Buffer.buffer("{" +
+                                                    "\"url\":\"" + feedUrl.getString("url") + "\"" +
+                                                    "}"));
+          }
+        }
+      }
+    );
+
+    /*
+
+    this.httpClient.post(4242, "localhost", "/auth/local/",
+                                    response -> {
+      response.handler(body -> {
+        System.out.println("response: " + body);
+      });
+    })
+    .end(Buffer.buffer("{" +
+                        "\"email\":\"farouko@justicier.com\"," +
+                        "\"password\":\"toto123\"" +
+                       "}"));
+     */
+  }
+
   public void refreshFeeds(Long id) {
     System.out.println("worker fired ! id: " + id);
+    if (this.httpClient == null) {
+      initFeeds();
+    }
   }
 
   public void setDelay(long delay) {
