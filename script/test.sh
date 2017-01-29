ca9649a26a945a7b4bd426164cf4980bc7d843b8
Marc B <marc.benzahra@gmail.com>
2017-01-29 22:50:44 +0100

    some tests

--- script/test.sh
+++ script/test.sh
@@ -1,1 +1,3 @@
+export SECRET=secret
+keytool -genseckey -keystore keystore.jceks -storetype jceks -storepass $SECRET -keyalg HMacSHA256 -keysize 2048 -alias HS256 -keypass $SECRET
 mvn test
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -28,20 +28,55 @@ public class ServerTest {
   }
 
   @Test
-  public void testMyApplication(TestContext context) {
+  public void credentialCreate(TestContext context) {
     final Async async = context.async();
 
-    vertx.createHttpClient().post(4242, "localhost", "/auth/local/",
-                                    response -> {
-      response.handler(body -> {
-        System.out.println("response: " + body);
-//        context.assertTrue(body.toString().contains("credential"));
-        async.complete();
-      });
+    vertx.createHttpClient().post(4242, "79.137.78.39", "/api/credentials/",
+                                  response -> {
+      context.assertEquals(response.statusCode(), 200);
+      async.complete();
     })
     .end(Buffer.buffer("{" +
-                        "\"email\":\"farouko@justicier.com\"," +
+                        "\"email\":\"test@justicier.com\"," +
                         "\"password\":\"toto123\"" +
+                       "}"));
+  }
+
+  @Test
+  public void articleCreate(TestContext context) {
+    final Async async = context.async();
+
+    vertx.createHttpClient().post(4242, "79.137.78.39", "/api/articles/",
+                                  response -> {
+      context.assertEquals(response.statusCode(), 200);
+      async.complete();
+    })
+    .end(Buffer.buffer("{" +
+                        "\"feedId\":\"1234\"," +
+                        "\"title\":\"bob\"," +
+                        "\"link\":\"google.fr\"," +
+                        "\"description\":\"lil info\"," +
+                        "\"pubDate\":\"28122006\"," +
+                        "\"author\":\"anthony\"," +
+                        "\"comments\":\"c cool\"" +
+                       "}"));
+  }
+
+  @Test
+  public void feedCreate(TestContext context) {
+    final Async async = context.async();
+
+    vertx.createHttpClient().post(4242, "79.137.78.39", "/api/articles/",
+                                  response -> {
+      context.assertEquals(response.statusCode(), 200);
+      async.complete();
+    })
+    .end(Buffer.buffer("{" +
+                        "\"url\":\"http://feeds.bbci.co.uk/news/world/rss.xml\"," +
+                        "\"title\":\"BBC News - World\"," +
+                        "\"link\":\"http://www.bbc.co.uk/news/\"," +
+                        "\"description\":\"BBC News - World\"," +
+                        "\"articles\":[\"28122006\", \"1234\"]" +
                        "}"));
   }
 }
